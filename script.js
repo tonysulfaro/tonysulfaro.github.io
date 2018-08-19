1fef0141786d13e166c4552205631ca18d418709
Tony Sulfaro <tony116523@gmail.com>
2018-08-19 16:32:58 -0400

    fixed typo, added image

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, {});
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
