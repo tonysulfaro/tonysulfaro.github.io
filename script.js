3f7d76ce47dd9580bedb68e0e9cf8c7969b5deb3
Tony Sulfaro <tony116523@gmail.com>
2018-09-11 23:21:38 -0400

    More mobile bugfixes

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
   });
 
-
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, {});
