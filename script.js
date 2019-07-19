0ec9af8a25561584e2f7bca863afef9cbe557d5f
Tony Sulfaro <tony116523@gmail.com>
2019-07-18 21:37:06 -0700

    add on hover behavior for link items

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("hey, you're not supposed to be here");
+
+var menu = document.getElementById('hamburger-menu');
+menu.addEventListener('click', function(){
+    var nav = document.getElementById('sidenav');
+    nav.style.visibility = 'hidden';
+});
+
+function toggleNav(){
+    var nav = document.getElementById('sidenav');
+    
+    if(nav.style.visibility === 'hidden'){
+        nav.style.visibility = 'visible';
+        nav.style.height = auto;
+        nav.style.margin = 0;
+        nav.style.padding = '4em 0 0 0';
+    }
+    else {
+        nav.style.visibility = 'hidden';
+        nav.style.height = 0;
+        nav.style.margin = 0;
+        nav.style.padding = 0;
+    }
+}
