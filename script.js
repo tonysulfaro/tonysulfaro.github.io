70e14ab2b25faa88f12b949cd1f088f8041fbd24
tony <tony116523@gmail.com>
2019-10-01 22:43:10 -0400

    fixed mobile nav

--- script.js
+++ script.js
@@ -2,27 +2,27 @@ console.log("hey, you're not supposed to be here");
 
 var toggleCount = 0;
 
-document.addEventListener('click', function(e){
-    if(e.target.tagName === 'A'){
+document.addEventListener('click', function (e) {
+    if (e.target.tagName === 'A') {
         toggleNav();
     }
-},false);
+}, false);
 
 function toggleNav() {
 
-    toggleCount ++;
+    toggleCount++;
 
     var nav = document.getElementById('mobile-sidenav');
 
     // not collapsed yet
-    if(nav.style.visibility == 'hidden' | toggleCount === 1){
+    if (nav.style.visibility == 'hidden' | toggleCount === 1) {
         nav.style.visibility = 'visible';
-        nav.style.margin = 0;
+        nav.style.margin = '4px 0 0 0';
         nav.style.padding = '0 0 2em 0';
-        nav.style.height = '100%';
+        nav.style.height = '600px';
     }
     // collapse elements
-    else{
+    else {
         nav.style.visibility = 'hidden';
         nav.style.margin = 0;
         nav.style.padding = 0;
