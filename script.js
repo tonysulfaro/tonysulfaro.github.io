6839d22716149cfeed91b2e391d1fba5d9b8eaa0
toby <tony116523@gmail.com>
2020-01-16 01:41:28 -0500

    update CNAME

--- script.js
+++ script.js
@@ -3,29 +3,29 @@ console.log("hey, you're not supposed to be here");
 var toggleCount = 0;
 
 document.addEventListener('click', function (e) {
-    if (e.target.tagName === 'A') {
-        toggleNav();
-    }
+  if (e.target.tagName === 'A') {
+    toggleNav();
+  }
 }, false);
 
 function toggleNav() {
 
-    toggleCount++;
+  toggleCount++;
 
-    var nav = document.getElementById('mobile-sidenav');
+  var nav = document.getElementById('mobile-sidenav');
 
-    // not collapsed yet
-    if (nav.style.visibility == 'hidden' | toggleCount === 1) {
-        nav.style.visibility = 'visible';
-        nav.style.margin = '4px 0 0 0';
-        nav.style.padding = '0 0 2em 0';
-        nav.style.height = '600px';
-    }
-    // collapse elements
-    else {
-        nav.style.visibility = 'hidden';
-        nav.style.margin = 0;
-        nav.style.padding = 0;
-        nav.style.height = 0;
-    }
+  // not collapsed yet
+  if (nav.style.visibility == 'hidden' | toggleCount === 1) {
+    nav.style.visibility = 'visible';
+    nav.style.margin = '4px 0 0 0';
+    nav.style.padding = '0 0 2em 0';
+    nav.style.height = '600px';
+  }
+  // collapse elements
+  else {
+    nav.style.visibility = 'hidden';
+    nav.style.margin = 0;
+    nav.style.padding = 0;
+    nav.style.height = 0;
+  }
 }
